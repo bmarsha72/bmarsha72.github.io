fedf0e22916945a87013fac0f4d8936a09cbae53
Brian Marshall <bmarsha72@gmail.com>
2016-03-08 15:25:12 -0600

    jim changes to update audio tags

--- project1/script/scripts.js
+++ project1/script/scripts.js
@@ -3,21 +3,30 @@ $(document).ready(function(){
 
   var mp3 = ['audio/golf.mp3','audio/china.mp3','audio/IslamicTerrorism.mp3','audio/the_middle_east.mp3']
 
-  var select = document.getElementById("selectNumber");
+  //var select = document.getElementById("selectNumber");
 
-  var options = ["golf", "China", "Islamic Terrorism", "The Middle East"];
+  //var options = ["golf", "China", "Islamic Terrorism", "The Middle East"];
 
-  for(var i = 0; i < options.length; i++) {
-      var opt = options[i];
-      var el = document.createElement("option");
-      el.textContent = opt;
-      el.value = 
-      select.appendChild(el);
-  }
+  //for(var i = 0; i < options.length; i++) {
+  //    var opt = options[i];
+  //    var el = document.createElement("option");
+  //    el.textContent = opt;
+  //    el.value =
+  //    select.appendChild(el);
+  //}
 
 //  $('#nounSelector').append('<option value="'+mp3[0]+'" >Golf</option>')
 //  $('#nounSelector').append('<option value="'+mp3[1]+'" >China</option>')
 //  $('#nounSelector').append('<option value="'+mp3[2]+'" >Islamic Terrorism</option>')
 //  $('#nounSelector').append('<option value="'+mp3[3]+'" >The Middle East</option>')
 
-});
+ $( '.test1' ).change(function() {
+      $('#noun1').prop("src", jimRules())
+    });
+}); // end of doc readdy
+
+
+function jimRules(){
+  var optionValue = $('option:selected').val();
+  return optionValue
+}
