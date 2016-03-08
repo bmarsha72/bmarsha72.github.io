edb698f773ce56aa898da6f4aee3a9e331baebed
Brian Marshall <bmarsha72@gmail.com>
2016-03-08 10:27:33 -0600

    added mp3 files to pulldown

--- project1/script/scripts.js
+++ project1/script/scripts.js
@@ -1,44 +1,21 @@
 $(document).ready(function(){
   // things to do when the page has fully loaded
 
-  // globals
-  var player    = document.getElementById ("player"),
-  var playlist  = document.getElementById ("playlist"),
-  var stop      = document.getElementById ("stop");
 
-  // functions
-  function playlistItemClick(clickedElement) {
-      var selected = playlist.querySelector(".selected");
 
-      if (selected) {
-          selected.classList.remove("selected");
-      }
+  var mp3 = ['audio/golf.mp3','audio/china.mp3','audio/IslamicTerrorism.mp3','audio/the_middle_east.mp3']
 
-      clickedElement.classList.add("selected");
-      player.src = clickedElement.getAttribute("/Users''''/sarahmarshall1/Desktop/Rosie.mp3");
-      player.play();
+  var select = document.getElementById("selectNumber");
+
+  var options = ["golf", "China", "Islamic Terrorism", "The Middle East"];
+
+  for(var i = 0; i < options.length; i++) {
+      var opt = options[i];
+      var el = document.createElement("option");
+      el.textContent = opt;
+      el.value = opt;
+      select.appendChild(el);
   }
 
-  function playNext() {
-      var selected = playlist.querySelector("li.selected");
-      if (selected && selected.nextSibling) {
-          playlistItemClick(selected.nextSibling);
-      }
-  }
-
-  // event listeners
-  stop.addEventListener("click", function () {
-      player.pause();
-  });
-  player.addEventListener("ended", playNext);
-  playlist.addEventListener("click", function (e) {
-      if (e.target && e.target.nodeName === "LI") {
-          playlistItemClick(e.target);
-      }
-  });​
-
-
-
-
-
+  $('#selectNumber').append('<option value="'+mp3[1]+'" >China</option>')
 });
