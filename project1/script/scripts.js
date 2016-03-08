f569d8222f29dd95cd72301a39d2267d2e6b618f
Brian Marshall <bmarsha72@gmail.com>
2016-03-08 13:24:51 -0600

    small changes to pulldown

--- project1/script/scripts.js
+++ project1/script/scripts.js
@@ -1,8 +1,6 @@
 $(document).ready(function(){
   // things to do when the page has fully loaded
 
-
-
   var mp3 = ['audio/golf.mp3','audio/china.mp3','audio/IslamicTerrorism.mp3','audio/the_middle_east.mp3']
 
   var select = document.getElementById("selectNumber");
@@ -13,9 +11,13 @@ $(document).ready(function(){
       var opt = options[i];
       var el = document.createElement("option");
       el.textContent = opt;
-      el.value = opt;
+      el.value = 
       select.appendChild(el);
   }
 
-  $('#selectNumber').append('<option value="'+mp3[1]+'" >China</option>')
+//  $('#nounSelector').append('<option value="'+mp3[0]+'" >Golf</option>')
+//  $('#nounSelector').append('<option value="'+mp3[1]+'" >China</option>')
+//  $('#nounSelector').append('<option value="'+mp3[2]+'" >Islamic Terrorism</option>')
+//  $('#nounSelector').append('<option value="'+mp3[3]+'" >The Middle East</option>')
+
 });
