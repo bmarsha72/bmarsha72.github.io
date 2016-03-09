94826158ad5ad0208bb9436963c858fdb787eec4
Brian Marshall <bmarsha72@gmail.com>
2016-03-09 10:55:49 -0600

    added second noun pulldown and adjective pulldown

--- project1/script/scripts.js
+++ project1/script/scripts.js
@@ -1,29 +1,9 @@
 $(document).ready(function(){
   // things to do when the page has fully loaded
 
-  //var mp3 = ['audio/golf.mp3','audio/china.mp3','audio/IslamicTerrorism.mp3','audio/the_middle_east.mp3']
-
-  //var select = document.getElementById("selectNumber");
-
-  //var options = ["golf", "China", "Islamic Terrorism", "The Middle East"];
-
-  //for(var i = 0; i < options.length; i++) {
-  //    var opt = options[i];
-  //    var el = document.createElement("option");
-  //    el.textContent = opt;
-  //    el.value =
-  //    select.appendChild(el);
-  //}
-
-//  $('#nounSelector').append('<option value="'+mp3[0]+'" >Golf</option>')
-//  $('#nounSelector').append('<option value="'+mp3[1]+'" >China</option>')
-//  $('#nounSelector').append('<option value="'+mp3[2]+'" >Islamic Terrorism</option>')
-//  $('#nounSelector').append('<option value="'+mp3[3]+'" >The Middle East</option>')
-
-
-function createPlayer(url, next) {
+function createPlayer() {
   var audio = document.createElement('audio');
-  audio.src = 'audio/adjectives/Disgusting.mp3'
+  audio.src = 'audio/adjectives/Slob.mp3'
   audio.addEventListener('ended', function() {
     // when the song stops
     console.log('test');
@@ -33,7 +13,7 @@ function createPlayer(url, next) {
       // when the song stops
       console.log('test2');
       var thirdAudio = document.createElement('audio');
-      thirdAudio.src = 'audio/secondnouns/the_middle_east.mp3';
+      thirdAudio.src = 'audio/secondnouns/IslamicTerrorism.mp3';
       thirdAudio.play();
 
       // create new audio + add src
@@ -49,14 +29,18 @@ createPlayer();
 
 
 
+  var clipsSelected = [];
+  // Noun
+  $('.test1').change(function() {
+    console.log('this function is happening');
+    noun1Selected.push($(this, ':selected').val());
+    console.log(noun1Selected, '<--- You should see an array here');
 
- $('.test1').change(function() {
-   console.log('this function is happening')
-      $('#noun1').prop("src", jimRules())
-    });
+
+  });
+
+  // Adjective listener
+  $('.adjective').change(function() {
+    //
+  })
 }); // end of doc readdy
-
-function jimRules(){
-  var optionValue = $('option:selected').val();
-  return optionValue
-}
